82bad14bfc29dfb726db2405c9c0b336a749336e
vikashiscool <vikash_parekh@yahoo.com>
2016-04-07 15:06:53 -0700

    did a thing

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func main() {
 
 
     //http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+
+     // calling http.Handle on the "/" pattern will act as a catch-all route, so we define that route last. http.FileServer returns an http.Handler so we use http.Handle to map a pattern string to a handler. The alternative method, http.HandleFunc, uses an http.HandlerFunc instead of an http.Handler. This may be more convenient, to think of handling routes via a function instead of an object.
     http.HandleFunc("/markdown", generateMarkdown)
     http.Handle("/", http.FileServer(http.Dir("public")))
     // http.ListenAndServe(":8080", nil)
